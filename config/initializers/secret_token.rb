4f25acb3055f11693d9d132ee2b1b4ba931c4d62
Andreas Kemkes <a5sk4s@macbook-pro.home>
2010-09-10 18:11:38 -0700

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthlogicExample::Application.config.secret_token = '0e9d4f1b60d0d9d65a98ad52ff9fee0ff7393b80e127f5a5af1135e4ad644707dbdb479712d533c0d35b5e7901d193f905fafc6861ff4eebc4372c76a5dff05e'
