a704704c568102bf99aa326c55c5c4812f66d549
Andreas Kemkes <a5sk4s@macbook-pro.home>
2010-09-13 16:05:00 -0700

    active_record_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-AuthlogicExample::Application.config.session_store :cookie_store, :key => '_authlogic_example_session'
+# AuthlogicExample::Application.config.session_store :cookie_store, :key => '_authlogic_example_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# AuthlogicExample::Application.config.session_store :active_record_store
+AuthlogicExample::Application.config.session_store :active_record_store
